e866d3e6692bf0df987cba86714867f90865d983
Karl Dyrhage <karl.dyrhage@icm.uu.se>
2019-02-25 16:09:18 +0100

    Added protease cleavage site prediction

---
+++ src/peptidecleavage.jl
@@ -0,0 +1,38 @@
+"""
+    cleave(seq::AminoAcidSequence)
+
+Predict cleavage sites. Returns a vector of cleavage site indices and a vector
+containing the resulting peptides. Currently only supports trypsin.
+"""
+function cleave(seq::AminoAcidSequence)
+    seq = copy(seq)
+    seq[end] != AA_Term && push!(seq, AA_Term)
+    peptides = AminoAcidSequence[]
+    cleavagesites = Int[]
+    last = 0
+    for i in 3:length(seq)-1
+        window = seq[i-1:i+1]
+        if trypsin(window)
+            push!(peptides, seq[last+1:i])
+            push!(cleavagesites, i)
+            last = i
+        end
+    end
+    last < lastindex(seq) && push!(peptides, seq[last:end-1])
+    return (inds = cleavagesites, peptides = peptides)
+end
+
+
+function trypsin(window)
+    if window == aa"MRP" || window == aa"WKP"
+        return true
+    elseif window == aa"CKD" || window == aa"DKD" ||
+           window == aa"CKH" || window == aa"CKY" ||
+           window == aa"CRK" || window == aa"RRR" ||
+           window == aa"RRH"
+        return false
+    elseif (window[2] == AA_K || window[2] == AA_R) && window[3] != AA_P
+        return true
+    end
+    return false
+end
