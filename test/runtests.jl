88c5e6928fd10f560f202046befb4e6d1787e1e2
Karl Dyrhage <karl.dyrhage@gmail.com>
2021-04-16 16:35:15 +0200

    Changed module name

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using Test
-using ProteinStats
+using BioPredictions
 using BioSequences
 
 seq = LongAminoAcidSeq([aa for aa in alphabet(AminoAcid)])
@@ -10,7 +10,7 @@ end
 
 @testset "Molecular weight" begin
     @test isapprox(weight(seq),
-                   weight(seq; ignoreunknown = true) + sum(map(aa -> ProteinStats.aminoacidweights[aa], [AA_X, AA_Z, AA_B, AA_J])))
+                   weight(seq; ignoreunknown = true) + sum(map(aa -> BioPredictions.aminoacidweights[aa], [AA_X, AA_Z, AA_B, AA_J])))
 end
 
 @testset "Isoelectric point" begin
@@ -21,7 +21,7 @@ end
 
 @testset "Cleavage sites" begin
     @test cleave(seq[1:20]) == (positions = [2, 12], peptides = LongAminoAcidSeq[aa"AR", aa"NDCQEGHILK", aa"MFPSTWYV"])
-    @test cleave(seq[1:20], ProteinStats.proteinaseK) == (positions = [1, 7, 10, 11, 14, 17, 18, 19], peptides = LongAminoAcidSeq[aa"A", aa"RNDCQE", aa"GHI", aa"L", aa"KMF", aa"PST", aa"W", aa"Y", aa"V"])
-    @test cleave(seq[1:20], ProteinStats.lysC) == (positions = [12], peptides = LongAminoAcidSeq[aa"ARNDCQEGHILK", aa"MFPSTWYV"])
-    @test cleave(seq[1:20], ProteinStats.proline_endopeptidase) == (positions = Int[], peptides = LongAminoAcidSeq[seq[1:20]])
+    @test cleave(seq[1:20], BioPredictions.proteinaseK) == (positions = [1, 7, 10, 11, 14, 17, 18, 19], peptides = LongAminoAcidSeq[aa"A", aa"RNDCQE", aa"GHI", aa"L", aa"KMF", aa"PST", aa"W", aa"Y", aa"V"])
+    @test cleave(seq[1:20], BioPredictions.lysC) == (positions = [12], peptides = LongAminoAcidSeq[aa"ARNDCQEGHILK", aa"MFPSTWYV"])
+    @test cleave(seq[1:20], BioPredictions.proline_endopeptidase) == (positions = Int[], peptides = LongAminoAcidSeq[seq[1:20]])
 end
