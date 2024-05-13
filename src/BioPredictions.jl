b229def09d81949a8e46ae38560ad0ec58685d1c
Karl Dyrhage <karl.dyrhage@gmail.com>
2024-05-13 11:44:15 +0200

    Added codon adaptation index

--- src/BioPredictions.jl
+++ src/BioPredictions.jl
@@ -1,14 +1,17 @@
 module BioPredictions
 
-export pI, weight, gravy, cleave
+export pI, weight, gravy, cleave, cai
 
 using BioSequences
 using StatsBase
+using GenomicAnnotations
+using Kmers
 
 include("tables.jl")
 include("pI.jl")
 include("molecularweight.jl")
 include("gravy.jl")
 include("peptidecleavage.jl")
+include("codonadaptationindex.jl")
 
 end
---
+++ src/codonadaptationindex.jl
@@ -0,0 +1,99 @@
+function codon_frequencies(chrs)
+    s = dna""
+    for gene in @genes(chrs, CDS, iscomplete(gene))
+        s *= sequence(gene)
+    end
+    cf = countmap(map(last, SpacedKmers{DNACodon}(s, 3)))
+end
+
+function optimal_codons(cf)
+    maxf(v) = maximum(map(c -> get(cf, DNACodon(c), 0.0), v))
+    oc = Dict(DNACodon("TTT") => maxf(["TTT", "TTC"]), # Phenylalanine
+              DNACodon("TTC") => maxf(["TTT", "TTC"]),
+              DNACodon("TTA") => maxf(["TTA", "TTG", "CTT", "CTC", "CTA", "CTG"]), # Leucine
+              DNACodon("TTG") => maxf(["TTA", "TTG", "CTT", "CTC", "CTA", "CTG"]),
+              DNACodon("CTT") => maxf(["TTA", "TTG", "CTT", "CTC", "CTA", "CTG"]),
+              DNACodon("CTC") => maxf(["TTA", "TTG", "CTT", "CTC", "CTA", "CTG"]),
+              DNACodon("CTA") => maxf(["TTA", "TTG", "CTT", "CTC", "CTA", "CTG"]),
+              DNACodon("CTG") => maxf(["TTA", "TTG", "CTT", "CTC", "CTA", "CTG"]),
+              DNACodon("ATT") => maxf(["ATT", "ATC", "ATA", "ATG"]), # Isoleucine
+              DNACodon("ATC") => maxf(["ATT", "ATC", "ATA", "ATG"]),
+              DNACodon("ATA") => maxf(["ATT", "ATC", "ATA", "ATG"]),
+              DNACodon("ATG") => maxf(["ATT", "ATC", "ATA", "ATG"]),
+              DNACodon("GTT") => maxf(["GTT", "GTC", "GTA", "GTG"]), # Valine
+              DNACodon("GTC") => maxf(["GTT", "GTC", "GTA", "GTG"]),
+              DNACodon("GTA") => maxf(["GTT", "GTC", "GTA", "GTG"]),
+              DNACodon("GTG") => maxf(["GTT", "GTC", "GTA", "GTG"]),
+              DNACodon("TCT") => maxf(["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"]), # Serine
+              DNACodon("TCC") => maxf(["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"]),
+              DNACodon("TCA") => maxf(["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"]),
+              DNACodon("TCG") => maxf(["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"]),
+              DNACodon("AGT") => maxf(["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"]),
+              DNACodon("AGC") => maxf(["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"]),
+              DNACodon("CCT") => maxf(["CCT", "CCC", "CCA", "CCG"]), # Proline
+              DNACodon("CCC") => maxf(["CCT", "CCC", "CCA", "CCG"]),
+              DNACodon("CCA") => maxf(["CCT", "CCC", "CCA", "CCG"]),
+              DNACodon("CCG") => maxf(["CCT", "CCC", "CCA", "CCG"]),
+              DNACodon("ACT") => maxf(["ACT", "ACC", "ACA", "ACG"]), # Threonine
+              DNACodon("ACC") => maxf(["ACT", "ACC", "ACA", "ACG"]),
+              DNACodon("ACA") => maxf(["ACT", "ACC", "ACA", "ACG"]),
+              DNACodon("ACG") => maxf(["ACT", "ACC", "ACA", "ACG"]),
+              DNACodon("GCT") => maxf(["GCT", "GCC", "GCA", "GCG"]), # Alanine
+              DNACodon("GCC") => maxf(["GCT", "GCC", "GCA", "GCG"]),
+              DNACodon("GCA") => maxf(["GCT", "GCC", "GCA", "GCG"]),
+              DNACodon("GCG") => maxf(["GCT", "GCC", "GCA", "GCG"]),
+              DNACodon("TAT") => maxf(["TAT", "TAC"]), # Tyrosine
+              DNACodon("TAC") => maxf(["TAT", "TAC"]),
+              DNACodon("TAA") => maxf(["TAA", "TAG", "TGA"]), # Stop
+              DNACodon("TAG") => maxf(["TAA", "TAG", "TGA"]),
+              DNACodon("TGA") => maxf(["TAA", "TAG", "TGA"]),
+              DNACodon("CAT") => maxf(["CAT", "CAC"]), # Histidine
+              DNACodon("CAC") => maxf(["CAT", "CAC"]),
+              DNACodon("CAA") => maxf(["CAA", "CAG"]), # Glutamine
+              DNACodon("CAG") => maxf(["CAA", "CAG"]),
+              DNACodon("AAT") => maxf(["AAT", "AAC"]), # Asparagine
+              DNACodon("AAC") => maxf(["AAT", "AAC"]),
+              DNACodon("AAA") => maxf(["AAA", "AAG"]), # Lysine
+              DNACodon("AAG") => maxf(["AAA", "AAG"]),
+              DNACodon("GAT") => maxf(["GAT", "GAC"]), # Aspartic acid
+              DNACodon("GAC") => maxf(["GAT", "GAC"]),
+              DNACodon("GAA") => maxf(["GAA", "GAG"]), # Glutamic acid
+              DNACodon("GAG") => maxf(["GAA", "GAG"]),
+              DNACodon("TGT") => maxf(["TGT", "TGC"]), # Cysteine
+              DNACodon("TGC") => maxf(["TGT", "TGC"]),
+              DNACodon("TGG") => maxf(["TGG"]), # Tryptophan
+              DNACodon("CGT") => maxf(["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"]), # Arginine
+              DNACodon("CGC") => maxf(["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"]),
+              DNACodon("CGA") => maxf(["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"]),
+              DNACodon("CGG") => maxf(["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"]),
+              DNACodon("AGA") => maxf(["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"]),
+              DNACodon("AGG") => maxf(["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"]),
+              DNACodon("GGT") => maxf(["GGT", "GGC", "GGA", "GGG"]), # Glycine
+              DNACodon("GGC") => maxf(["GGT", "GGC", "GGA", "GGG"]),
+              DNACodon("GGA") => maxf(["GGT", "GGC", "GGA", "GGG"]),
+              DNACodon("GGG") => maxf(["GGT", "GGC", "GGA", "GGG"]))
+end
+
+function relative_adaptiveness(codon, cf, oc)
+    get(cf, codon, 0.0) / get(oc, codon, 0.0)
+end
+
+function cai(gene, cf, oc)
+    codons = SpacedKmers{DNACodon}(sequence(gene), 3)
+    w = fill(0.0, length(collect(codons)))
+    for (i, codon) in enumerate(codons)
+        w[i] = relative_adaptiveness(codon[2], cf, oc)
+    end
+    geomean(w)
+end
+
+function cai(chrs)
+    cf = codon_frequencies(chrs)
+    oc = optimal_codons(cf)
+    genes = @genes(chrs, CDS, iscomplete(gene))
+    result = fill(0.0, length(genes))
+    for (i, gene) in enumerate(genes)
+        result[i] = cai(gene, cf, oc)
+    end
+    result
+end
